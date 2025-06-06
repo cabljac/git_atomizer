aec570f9aca8b79208da6e5f17cd28f0b3d021a3
Jacob Cable <jacobcable94@gmail.com>
2025-06-06 12:39:41 +0100

    feat: get commit message on branch

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use clap::Parser;
-use gix::{self, refs::FullName};
+use gix::{self};
 
 /// test description!
 #[derive(Parser, Debug)]
@@ -13,7 +13,7 @@ struct Cli {
 fn main() {
     let args = Cli::parse();
 
-    let _branch = if let Some(branch_name) = args.branch {
+    let branch = if let Some(branch_name) = args.branch {
         println!("Specified branch: {}", branch_name);
         branch_name
     } else {
@@ -40,10 +40,67 @@ fn main() {
         }
     };
 
+    let mut reference = match repo.find_reference(&branch) {
+        Ok(reference) => reference,
+        Err(e) => {
+            println!("Error: cannot find reference for branch, {}", e);
+            return;
+        }
+    };
+
+    let commit = match reference.peel_to_commit() {
+        Ok(commit) => commit,
+        Err(e) => {
+            println!("Error: could not peel, {}", e);
+            return;
+        }
+    };
+
+    let message = match commit.message() {
+        Ok(message) => message,
+        Err(e) => {
+            println!("Error: could not get message from commit {}", e);
+            return;
+        }
+    };
+
+    println!("Latest commit on specified branch: {}", message.summary());
+    // or
+    println!("Latest commit message from specified branch: {:?}", message);
+
     if let Some(head_name) = head_name {
         let head_str = head_name.to_string();
 
         let branch_name = head_str.strip_prefix("refs/heads/").unwrap_or(&head_str);
         println!("On branch {}", branch_name);
+
+        let mut reference = match repo.find_reference(&head_name) {
+            Ok(reference) => reference,
+            Err(e) => {
+                println!("Error: could not find reference, {}", e);
+                return;
+            }
+        };
+
+        let commit = match reference.peel_to_commit() {
+            Ok(commit) => commit,
+            Err(e) => {
+                println!("Error: could not peel, {}", e);
+                return;
+            }
+        };
+
+        let message = match commit.message() {
+            Ok(message) => message,
+            Err(e) => {
+                println!("Error: could not get message from commit {}", e);
+                return;
+            }
+        };
+
+        // Add this:
+        println!("Latest commit on head: {}", message.summary());
+        // or
+        println!("Latest commit message from head: {:?}", message);
     }
 }
