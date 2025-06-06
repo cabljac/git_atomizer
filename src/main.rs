be03dbace29b6ef67903f6b7712060aae5dc09c2
Jacob Cable <jacobcable94@gmail.com>
2025-06-06 12:39:41 +0100

    feat: detect if in git repo

--- src/main.rs
+++ src/main.rs
@@ -13,11 +13,16 @@ struct Cli {
 fn main() {
     let args = Cli::parse();
 
-    let branch = if let Some(branch_name) = args.branch {
+    let _branch = if let Some(branch_name) = args.branch {
         println!("Specified branch: {}", branch_name);
         branch_name
     } else {
         println!("No branch specified, using main");
         String::from("main")
     };
+
+    match gix::discover(".") {
+        Ok(_repo) => println!("Found a git repository!"),
+        Err(e) => println!("Error: Not in a git repository - {}", e),
+    }
 }
